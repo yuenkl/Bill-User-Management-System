f9561b52f6e71fc2566460391233c3663bf2b81a
Bill <yuenkl2012@gmail.com>
2026-08-27 13:49:00 +0100

    Fix iOS build on Xcode 16

--- shared/src/androidHostTest/kotlin/com/bill/usermanagmentsystem/ui/theme/UserManagementThemeTest.kt
+++ shared/src/androidHostTest/kotlin/com/bill/usermanagmentsystem/ui/theme/UserManagementThemeTest.kt
@@ -8,7 +8,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.test.ExperimentalTestApi
-import androidx.compose.ui.test.v2.runComposeUiTest
+import androidx.compose.ui.test.runComposeUiTest
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import kotlin.test.Test
